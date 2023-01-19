92132174a5687bdee22600c2d55f265572ec4b64
barituleera <barituleera@gmail.com>
2023-01-19 13:46:20 +0100

    app_url fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('homeAsset/theme-assets/css/demo.min.css') }}">
     <!-- END CRYPTO CSS-->
     <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('homeAsset/theme-assets/css/template-3d-graphics.min.css') }}">
     <!-- END Page Level CSS-->
     <!-- BEGIN Custom CSS-->
