a13b1470f3016ff1065a8cf700fe3b3e6bad4aac
barituleera <barituleera@gmail.com>
2023-01-19 15:58:46 +0100

    adding Admin seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => 'Admin',
+            'last_name' => "Mike",
+            'email' =>  'admin@gmail.com', //$faker->unique()->safeEmail,
+            'phone' =>  '0987654321',
+            'country' =>  '0987654321',
+            'email_verified_at' => now(),
+            'password' => Hash::make('Admin@12345'), // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
