476cd4f830188007c2486bc197ad1dd445bcf525
barituleera <barituleera@gmail.com>
2023-01-20 18:09:12 +0100

    Reg fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
 
                                     </div>
 
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 mb-5">
 
                                         @if (Route::currentRouteName() == 'register')
                                         <a href="{{ route('login') }}" data-panel=".panel-signup" href="#"
