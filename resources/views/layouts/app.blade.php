48bf83cac339a0a85c89365379acba87515e6f13
barituleera <barituleera@gmail.com>
2023-01-20 18:07:56 +0100

    Reg fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
 
                                         @if (Route::currentRouteName() == 'register')
                                         <a href="{{ route('login') }}" data-panel=".panel-signup" href="#"
-                                        class="btn btn-block btn-facebook lnk-toggler"
+                                        class="btn btn-block btn-facebook mb-5 lnk-toggler"
                                         style="background-color:#FF4444;color:white;font-weight:bolder">
                                         <center>
                                             <i class="fa fa-reg"></i> <span class="">Login Now</span>
@@ -85,7 +85,7 @@
                                     @else
 
                                     <a href="{{ route('register') }}" data-panel=".panel-signup" href="#"
-                                            class="btn btn-block btn-facebook lnk-toggler"
+                                            class="btn btn-block btn-facebook mb-5 lnk-toggler"
                                             style="background-color:#FF4444;color:white;font-weight:bolder">
                                             <center>
                                                 <i class="fa fa-reg"></i> <span class="">Register Now</span>
