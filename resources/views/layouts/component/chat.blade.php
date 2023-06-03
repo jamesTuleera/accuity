2c6d7b6eb75b0139789a3e9c798e8255d4db1dfe
barituleera <barituleera@gmail.com>
2023-06-03 11:33:39 +0100

    changing chat

--- resources/views/layouts/component/chat.blade.php
+++ resources/views/layouts/component/chat.blade.php
@@ -1,2 +1,4 @@
-<script src="//code.tidio.co/c63hb18zfmjhnocn2flsdkactvcsp3zf.js" async></script>
+{{-- old from mastermindsfinnces --}}
+{{-- <script src="//code.tidio.co/c63hb18zfmjhnocn2flsdkactvcsp3zf.js" async></script> --}}
 
+<script src="//code.tidio.co/u1oayobo0brkhvhg2q50vmwuppbtmv46.js" async></script>
