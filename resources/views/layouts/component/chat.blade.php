396fea037e7dab4f1d4aa3cdcd7664cf605e49bc
barituleera <barituleera@gmail.com>
2023-12-02 18:31:19 +0100

    live chat

--- resources/views/layouts/component/chat.blade.php
+++ resources/views/layouts/component/chat.blade.php
@@ -1,3 +1,1 @@
-
-{{-- <script src="//code.tidio.co/u1oayobo0brkhvhg2q50vmwuppbtmv46.js" async></script> --}}
-<script src="//code.tidio.co/sjojbyzuaiwn6hx9dey7gyve6ijgmcq4.js" async></script>
+<script src="//code.tidio.co/plqemzwbsybfn9l47h043v9gma8pzig4.js" async></script>
